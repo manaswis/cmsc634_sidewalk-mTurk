6c7a82b7ed483301b597305dcf2143100a366a97
misaugstad <michaelssaugstad@gmail.com>
2017-05-14 11:37:57 -0400

    fixed typo

--- scripts/accuracy_calculations.py
+++ scripts/accuracy_calculations.py
@@ -40,21 +40,21 @@ f_measure_binary = precision_binary * recall_binary / (precision_binary + recall
 # positive, if nothing is close enough with same label type it is false
 # negative, and any turker labels not matched to a ground truth label (including
 # the ones that were close enough but had wrong type) are false positives.
-turk_left_mutli = turker_labels.copy()
+turk_left_multi = turker_labels.copy()
 true_positive_multi = 0
 false_negative_multi = 0
 for row in ground_truth.iterrows():
 	# get distance from this label in ground truth to every remaining turker label w/ same type
-	same_type_labels = turk_left_mutli[turk_left_mutli.type == row[1].type]
+	same_type_labels = turk_left_multi[turk_left_multi.type == row[1].type]
 	dists = same_type_labels.apply(lambda x: haversine(x.coords, row[1].coords), axis=1)
 	# check if the closest one is less than 0.5 meters away, if so it is true pos o/w false neg
 	if dists.loc[dists.idxmin()] < 0.5:
 		true_positive_multi += 1
-		turk_left_mutli = turk_left_mutli.drop(dists.idxmin())
+		turk_left_multi = turk_left_multi.drop(dists.idxmin())
 	else:
 		false_negative_multi += 1
 
-false_positive_multi = len(turk_left_mutli)
+false_positive_multi = len(turk_left_multi)
 
 # calculate precision, recall, and f-measure
 precision_multi = true_positive_multi / (1.0*true_positive_multi + false_positive_multi)
