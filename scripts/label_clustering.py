d06ea104fa410e585377aae59932014ffcd283ef
misaugstad <michaelssaugstad@gmail.com>
2017-05-17 21:05:20 -0400

    added label_id to labels query

--- scripts/label_clustering.py
+++ scripts/label_clustering.py
@@ -23,7 +23,7 @@ else:
 		exit()
 
 # read in data
-names = ['lng','lat','label_type','asmt_id','turker_id','route_id','hit_id','pano_id','completed']
+names = ['lng','lat','label_type', 'label_id','asmt_id','turker_id','route_id','hit_id','pano_id','completed']
 label_data = pd.read_csv('../data/mturk_labels.csv', names=names)
 
 # subset data to remove onboarding pano ids
