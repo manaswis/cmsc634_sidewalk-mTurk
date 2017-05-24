9c05d734e0573130d2cb87b1d04cec48d7af5035
misaugstad <michaelssaugstad@gmail.com>
2017-05-24 11:27:17 -0400

    changed layout of GT tool

--- ground_truth_tool/ui.R
+++ ground_truth_tool/ui.R
@@ -4,15 +4,11 @@ library(googleway)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  # Application title
   titlePanel("Let's resolve some conflicts!"),
   
-  sidebarLayout(
-    mainPanel(
-      uiOutput(outputId="sv"),
-      google_mapOutput("myMap", height=600),
-      actionButton("test.button","click me")
-    ),
-    sidebarPanel("This is where words will go")
-  )
+  fluidRow(
+    column(6, google_mapOutput("myMap", height=600)),
+    column(6, uiOutput(outputId="sv"))),
+  hr(),
+  actionButton("test.button","click me")
 ))
